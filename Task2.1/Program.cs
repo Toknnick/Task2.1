46fb90aaba8ada6c09178bdcb368e9a51b2140ba
Maksim <maksimus.tumannick@yandex.ru>
2022-06-24 14:56:11 +0300

    Task2.1

---
+++ Task2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение");
+            string message = Console.ReadLine();
+            Console.WriteLine("Введите кол-во повторений");
+            int timesOfRepeat = int.Parse(Console.ReadLine());
+            for(int i = 0; i < timesOfRepeat;i++) 
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
